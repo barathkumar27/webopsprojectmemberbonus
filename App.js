a52cbe08a463208adbd699caa8e8f219215d58b4
barathkumar27 <79106661+barathkumar27@users.noreply.github.com>
2021-07-20 23:16:36 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+//import './App.css';
+//import Home from './components/title.jsx';
+//import Navbar from './components/navbar.jsx';
+import Forms from './components/form2.jsx';
+
+function App() {
+  return (
+    <div>
+      < Forms />
+    </div>
+  );
+}
+
+export default App;
