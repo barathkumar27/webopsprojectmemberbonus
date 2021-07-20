990cbd4e6cc25c6fbb731f0534461323e5034d2d
barathkumar27 <79106661+barathkumar27@users.noreply.github.com>
2021-07-20 23:17:48 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
-//import './App.css';
-//import Home from './components/title.jsx';
-//import Navbar from './components/navbar.jsx';
-import Forms from './components/form2.jsx';
+
+import Forms from './form2.jsx';
 
 function App() {
   return (
